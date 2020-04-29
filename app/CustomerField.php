b15efb45902481c76325698f0fc8b5f4e6e0d922
AmujoDotun <samuelamujo@gmail.com>
2020-04-29 02:27:20 +0100

    created customerfield model and migration filled it and loop through each field in order to collect input and save to the db

---
+++ app/CustomerField.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerField extends Model
+{
+    protected $fillable = ['customer_id', 'field_key', 'field_value'];
+}
--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Customer;
+use App\CustomerField;
 use App\Invoice;
+use App\InvoiceItem;
 
 class InvoiceController extends Controller
 {
@@ -16,7 +18,32 @@ class InvoiceController extends Controller
     public function store(Request $request)
     {
         $customer = Customer::create($request->customer);
-        Invoice::create($request->invoice + ['customer_id' => $customer->id]);
+        $invoice = Invoice::create($request->invoice + ['customer_id' => $customer->id]);
+
+        for ($i=0; $i< count($request->product); $i++)
+        {
+            if(isset($request->qty[$i]) && isset($request->price[$i]))
+            {
+                InvoiceItem::create([
+                    'invoice_id' => $invoice->id,
+                    'name' => $request->product[$i],
+                    'quality' => $request->qty[$i],
+                    'price' => $request->price[$i]
+                ]);
+            }
+        }
+
+        for ($i=0; $i< count($request->customer_fields); $i++)
+        {
+            if(isset($request->customer_fields[$i]['field_key']) && isset($request->customer_fields[$i]['field_value']))
+            {
+                CustomerField::create([
+                    'customer_id' => $customer->id,
+                    'field_key' => $request->customer_fields[$i]['field_key'],
+                    'field_value' => $request->customer_fields[$i]['field_value'] 
+                ]);
+            }
+        }
         return 'to be continue';
     }
 }
